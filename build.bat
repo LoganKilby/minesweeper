8e4e3eb14051fb08155c468bdc202aadf774f636
Logan Kilby <logankilby@gmail.com>
2021-07-10 22:35:01 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set opts=-FC -GR- -EHa- -nologo -Zi -DASSERTIONS_ENABLED=1 -DDEBUG_MODE=1
+set opts=-FC -GR- -EHa- -nologo -Zi -DASSERTIONS_ENABLED=1
 set code=%cd%
 pushd build
 cl %opts% %code%\win32_minesweeper.cpp -Feminesweeper /link /LIBPATH:..\lib raylibdll.lib winmm.lib
